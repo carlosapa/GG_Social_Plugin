5f4134f46bb3c5a80955f0f194baec93fec676b4
carlosapa <carlos_non86@msn.com>
2014-12-02 18:36:19 +0100

    New Images and Function to Array

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $global_name = 'Portfolio';
 function add_linked_in_metabox () {
 	add_meta_box(
 		'linked_in_box', 
-		esc_html__('LinkedIn URL', 'linkedin_plugin' ),
+		esc_html__('Social URLs', 'linkedin_plugin' ),
 		'linked_in_box_cb',
 		$global_name,
 		'normal',
@@ -31,52 +31,99 @@ function add_linked_in_metabox () {
 
 /* display meta box */
 function linked_in_box_cb ($object, $box) {
-	wp_nonce_field( basename( __FILE__ ), 'linked_in_box_nonce' );
-	wp_nonce_field( basename( __FILE__ ), 'facebook_in_box_nonce' );
-	wp_nonce_field( basename( __FILE__ ), 'twitter_in_box_nonce' );
-?>
+	wp_nonce_field( basename( __FILE__ ), 'linked_in_box_nonce' ); ?>
+
 	<p>
-	    <label for="linked_in_box"><?php _e( "Add your LinkedIn's personal site URL.", 'linkedin_plugin' ); ?></label>
+	    <label for="linked_in_box">
+	    	<img src="<?php echo plugin_dir_url(__FILE__); ?>img/linked_in-tiny.png"
+	    		 style="margin: 2px 4px -2px 0;" />
+	    	<?php _e( "Add your LinkedIn's personal site URL.", 'linkedin_plugin' ); ?>
+	    </label>
 	    <br />
-	    <input class="widefat" type="text" name="linked_in_box" id="linked_in_box" value="<?php echo esc_url(get_post_meta( $object->ID, 'linked_in_box', true )); ?>" size="30" placeholder="your URL here, please..."/>
+	    <input class="widefat" type="text" name="linked_in_box" 
+	    	   id="linked_in_box" value="<?php echo esc_url(get_post_meta( $object->ID, 'linked_in_box', true )); ?>" 
+	    	   size="30" placeholder="your URL here, please..."
+	    	   style="height:35px; margin-top: 4px; border-radius: 2px;"/>
   	</p>
   	<p>
-	    <label for="twitter_in_box"><?php _e( "Add your Twitter's personal site URL.", 'linkedin_plugin' ); ?></label>
+	    <label for="twitter_in_box">
+	    	<img src="<?php echo plugin_dir_url(__FILE__); ?>img/twitter_in-tiny.png"
+	    		 style="margin: 2px 4px -2px 0;" />
+	    	<?php _e( "Add your Twitter's personal site URL.", 'linkedin_plugin' ); ?>
+		</label>
 	    <br />
-	    <input class="widefat" type="text" name="twitter_in_box" id="twitter_in_box" value="<?php echo esc_url(get_post_meta( $object->ID, 'twitter_in_box', true )); ?>" size="30" placeholder="your URL here, please..."/>
+	    <input class="widefat" type="text" name="twitter_in_box" 
+	           id="twitter_in_box" value="<?php echo esc_url(get_post_meta( $object->ID, 'twitter_in_box', true )); ?>" 
+	           size="30" placeholder="your URL here, please..."
+	           style="height:35px; margin-top: 4px; border-radius: 2px;"/>
   	</p>
   	<p>
-	    <label for="facebook_in_box"><?php _e( "Add your Facebook's personal site URL.", 'linkedin_plugin' ); ?></label>
+	    <label for="xing_in_box">
+	    	<img src="<?php echo plugin_dir_url(__FILE__); ?>img/xing_in-tiny.png"
+	    		 style="margin: 2px 4px -2px 0;" />
+	    	<?php _e( "Add your Xing's personal site URL.", 'linkedin_plugin' ); ?>
+		</label>
 	    <br />
-	    <input class="widefat" type="text" name="facebook_in_box" id="facebook_in_box" value="<?php echo esc_url(get_post_meta( $object->ID, 'facebook_in_box', true )); ?>" size="30" placeholder="your URL here, please..."/>
+	    <input class="widefat" type="text" name="xing_in_box" 
+	    	   id="xing_in_box" value="<?php echo esc_url(get_post_meta( $object->ID, 'xing_in_box', true )); ?>" 
+	    	   size="30" placeholder="your URL here, please..."
+	    	   style="height:35px; margin-top: 4px; border-radius: 2px;"/>
   	</p>
+  	<p>
+	    <label for="facebook_in_box">
+	    	<img src="<?php echo plugin_dir_url(__FILE__); ?>img/facebook_in-tiny.png"
+	    		 style="margin: 2px 4px -2px 0;" />
+	    	<?php _e( "Add your Facebook's personal site URL.", 'linkedin_plugin' ); ?>
+		</label>
+	    <br />
+	    <input class="widefat" type="text" name="facebook_in_box" 
+	    	   id="facebook_in_box" value="<?php echo esc_url(get_post_meta( $object->ID, 'facebook_in_box', true )); ?>" 
+	    	   size="30" placeholder="your URL here, please..."
+	    	   style="height:35px; margin-top: 4px; border-radius: 2px;"/>
+  	</p>
+  	<p>
+	    <label for="vcard_in_box">
+	    	<img src="<?php echo plugin_dir_url(__FILE__); ?>img/vcard_in-tiny.png"
+	    		 style="margin: 2px 4px -2px 0;" />
+	    	<?php _e( "Add your VCard URL.", 'linkedin_plugin' ); ?>
+		</label>
+	    <br />
+	    <input class="widefat" type="text" name="vcard_in_box" 
+	    	   id="vcard_in_box" value="<?php echo esc_url(get_post_meta( $object->ID, 'vcard_in_box', true )); ?>" 
+	    	   size="30" placeholder="your URL here, please..."
+	    	   style="height:35px; margin-top: 4px; border-radius: 2px;"/>
+  	</p>
+
 <?php
 }
 
 /* save meta box */
 function save_linked_in_metabox ($post_id, $post) {
 
+	if (!current_user_can($post_type->cap->edit_post, $post_id)) { return $post_id; }
+	if ( !isset($_POST['linked_in_box_nonce']) || !wp_verify_nonce('linked_in_box_nonce' , basename( __FILE__ )) ) 
+		return $post_id;
 
-
-	$fields_array = array('linked', 'twitter', 'facebook');
+	$field = array('linked', 'twitter', 'xing', 'facebook', 'vcard');
+	$fields_size = sizeof($field);
 	$post_type = get_post_type_object( $post->post_type );
 
-	if ( !current_user_can( $post_type->cap->edit_post, $post_id ) ) { return $post_id; }
+	for ($i = 0; $i < $fields_size; $i++) {
+			
+		$actual_field   = $field[$i] . '_in_box';
+		$new_meta_value = (isset($_POST[$actual_field])) ? $_POST[$actual_field] : '';
+		$meta_key 		= $actual_field;
+		$meta_value 	= get_post_meta($post_id, $meta_key, true);
 
-	foreach ($fields_array as $field) {
+		if ($new_meta_value && '' == $meta_value) {
+			add_post_meta($post_id, $meta_key, $new_meta_value, true);
+
+		} elseif ($new_meta_value && $new_meta_value != $meta_value) {
+			update_post_meta($post_id, $meta_key, $new_meta_value);
+
+		} elseif ('' == $new_meta_value && $meta_value) { 
+			delete_post_meta($post_id, $meta_key, $meta_value);
 		
-		if ( !isset( $_POST[$field . '_box_nonce'] ) || !wp_verify_nonce($field . '_box_nonce', basename( __FILE__ ) ) ) { return $post_id; }
-
-		$new_meta_value = ( isset( $_POST[$field . '_box'] ) ? $_POST[$field . '_box'] : '' );
-		$meta_key = $field . '_box';
-		$meta_value = get_post_meta( $post_id, $meta_key, true );
-
-		if ( $new_meta_value && '' == $meta_value ) {
-			add_post_meta( $post_id, $meta_key, $new_meta_value, true );
-		} elseif ( $new_meta_value && $new_meta_value != $meta_value ) {
-			update_post_meta( $post_id, $meta_key, $new_meta_value );
-		} elseif ( '' == $new_meta_value && $meta_value ) { 
-			delete_post_meta( $post_id, $meta_key, $meta_value );
 		} 	
 	}
 }
@@ -96,41 +143,51 @@ add_action( 'load-post-new.php', 'linked_in_metabox_setup' );
 
 function the_linkedin_URL_cb () {
 
-    function find_the_URL () {
+    function find_the_URL ($string_in) {
         
         $orig_bid = get_current_blog_id();
         $post_slug = get_the_title(get_the_id());
         $bids = array(1, 2, 3, 4, 5, 6);
-        $linkedin_URL_output = '';
+        $URL_output = '';
+        $key = $string_in . '_in_box';
 
-        foreach ($bids as $bid) {
-          switch_to_blog($bid);
-          $args = array('post_type' => 'portfolio', 'meta_key' => 'linked_in_box');
-          $query = new WP_Query( $args );
-          $size = sizeof($query->posts);
-          
-          for ($i = 0; $i < $size; $i++) {
-            $title = $query->posts[$i]->post_title;
-            if ($title == $post_slug) {
-              $linkedin_URL_output = get_post_meta( $query->posts[$i]->ID, 'linked_in_box', true );
-            }
-          }
-          
-          wp_reset_query();
+     	// check if the url exists
+        if (get_post_meta(get_the_id(), $key, true ) !== '') {
+
+        	$URL_output = get_post_meta(get_the_id(), $key, true );
+
+        } else {
+
+	        foreach ($bids as $bid) {
+				switch_to_blog($bid);
+				$args = array('post_type' => 'portfolio', 'meta_key' => $key);
+				$query = new WP_Query( $args );
+				$size = sizeof($query->posts);
+
+				for ($j = 0; $j < $size; $j++) {
+					$title = $query->posts[$j]->post_title;
+					if ($title == $post_slug) {
+						$URL_output = get_post_meta( $query->posts[$j]->ID, $key, true );
+					}
+				}
+
+				wp_reset_query();
+	        }
+
+	        restore_current_blog();
         }
-        
-        restore_current_blog();
-        return $linkedin_URL_output;
+
+        return $URL_output;
     }
 
     function display_the_URL () {
-        $URL = find_the_URL();
+        $URL = find_the_URL('linked');
         echo $URL;
     }
     
 
     function the_URL () {
-        $URL = find_the_URL();
+        $URL = find_the_URL('linked');
         if ($URL !== '') {
             $string_output = '
               <a href="' . $URL . '" title="linked_in_URL" target="_blank">
@@ -138,6 +195,27 @@ function the_linkedin_URL_cb () {
               </a>';
             echo $string_output;
         }
+    }
+
+    function the_Social_Block () {
+    	$social_media = array('linked', 'twitter', 'xing', 'facebook', 'vcard');
+    	$social_media_length = sizeof($social_media);
+    	$URL = array();
+
+    	for ($i = 0; $i < $social_media_length; $i++) {
+    		
+    		$URL[$i] = find_the_URL($social_media[$i]);
+
+    		if ($URL[$i] !== '') {
+	            $string_output = '
+	              <a href="' . $URL[$i] . '" title="linked_in_URL" target="_blank">
+	                <img src="' . plugin_dir_url(__FILE__) . '/img/logo_cards-' . $social_media[$i] . '.png"/>
+	              </a>';
+	            echo $string_output;
+	        }
+    	}
+
+    	?><!--<pre><?php //print_r($URL); ?></pre>--><?php
     }
 }
 
